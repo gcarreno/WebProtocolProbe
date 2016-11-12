e356885287ec259b68acf3fda62f999e7cf8d9ff
Gustavo Carreno <guscarreno@gmail.com>
2016-11-12 17:23:17 +0000

    Using TCollection and TCollectionItem for Projects.

--- src/gc.webprotocolprobe.projects.pas
+++ src/gc.webprotocolprobe.projects.pas
@@ -9,26 +9,65 @@ uses
 
 type
 { TWPPProject }
-  TWPPProject = class(TOject)
+  TWPPProject = class(TCollectionItem)
   private
     FName: String;
     FDescription: String;
     FFileName: String;
   protected
   public
+    constructor Create(ACollection: TCollection); override;
+  published
     property Name: String read FName write FName;
     property Description: String read FDescription write FDescription;
     property FileName: String read FFileName write FFileName;
   end;
 
 { TWPPProjects }
-  TWPPProjects = class(TObject)
+  TWPPProjects = class(TCollection)
   private
+    function GetItems(Index: Integer): TWPPProject;
+    procedure SetItems(Index: Integer; Value: TWPPProject);
   protected
   public
+    constructor Create;
+
+    function Add: TWPPProject;
+
+    property Items[Index: Integer]: TWPPProject read GetItems write SetItems; default;
   end;
 
 implementation
+
+{ TWPPProject }
+
+constructor TWPPProject.Create(ACollection: TCollection);
+begin
+	if Assigned(ACollection) then
+		inherited Create(ACollection);
+end;
+
+{ TWPPProjects }
+
+function TWPPProjects.GetItems(Index: Integer): TWPPProject;
+begin
+  Result:= inherited Items[Index] as TWPPProject;
+end;
+
+procedure TWPPProjects.SetItems(Index: Integer; Value: TWPPProject);
+begin
+  Items[Index].Assign(Value);
+end;
+
+constructor TWPPProjects.Create;
+begin
+   inherited Create(TWPPProject);
+end;
+
+function TWPPProjects.Add: TWPPProject;
+begin
+  Result:= inherited Add as TWPPProject;
+end;
 
 end.
 
